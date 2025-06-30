a074f83a1459d1d38822ec5beb604646ad9f9169
rohit-35 <rsnayal8@gmail.com>
2025-06-30 22:15:32 +0530

    project commit

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const store = MongoStore.create({
 store.on("error", () => {
     console.log("ERROR in MONGO SESSION STORE", err);
 });
-
+ 
 const sessionOptions = {
     store: store,
     secret: process.env.SECRET,
